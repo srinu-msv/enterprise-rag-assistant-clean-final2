f6dfb32338ca928c593322ea20ed8bae8b8a7486
raju <raju@gmail.com>
2026-06-28 16:01:33 +0530

    Added Dockerfile and finalized dependencies

--- utils/chatbot.py
+++ utils/chatbot.py
@@ -1,34 +1,33 @@
 import os
 from dotenv import load_dotenv
-from langchain_groq import ChatGroq
+from langchain_openai import ChatOpenAI
 
+# Load environment variables
 load_dotenv()
 
-llm = ChatGroq(
-    api_key=os.getenv("GROQ_API_KEY"),
-    model="llama-3.3-70b-versatile",
+# Initialize OpenAI LLM
+llm = ChatOpenAI(
+    api_key=os.getenv("OPENAI_API_KEY"),
+    model="gpt-4o-mini",  # or "gpt-4o" / "gpt-4-turbo"
     temperature=0
 )
 
 def generate_answer(question, context):
-
     prompt = f"""
-You are an Enterprise Financial Intelligence Assistant.
+    You are an Enterprise Financial Intelligence Assistant.
 
-Answer the user's question using only the context provided.
+    Answer the user's question using only the context provided.
 
-If the answer is not available in the context, reply:
-"I couldn't find the answer in the uploaded document."
+    If the answer is not available in the context, reply:
+    "I couldn't find the answer in the uploaded document."
 
-Context:
-{context}
+    Context:
+    {context}
 
-Question:
-{question}
+    Question:
+    {question}
 
-Answer:
-"""
-
+    Answer:
+    """
     response = llm.invoke(prompt)
-
     return response.content
